71ddc3d0aaee64549ff1a31d428f30e4b85652d1
isilaygamze <igtanchor1@gmail.com>
2026-01-02 22:53:16 +0300

    Bildirim mail adresi düzeltildi

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
 
     const data = await resend.emails.send({
       from: 'AhsapKes <onboarding@resend.dev>', // Ücretsiz planda bu adres sabit kalmalı
-      to: ['igtanchor1@gmail.com'], // Buraya bildirim almak istediğin kendi mailini yaz
+      to: ['isilaygamzetoluk@gmail.com'], // Buraya bildirim almak istediğin kendi mailini yaz
       subject: 'Yeni Teklif Talebi Var! 🪵',
       html: `<p><strong>${email}</strong> adresinden yeni bir teklif talebi geldi.</p>
              <p>Dosya Linki: <a href="${fileUrl}">${fileUrl}</a></p>`
